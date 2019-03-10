2bc43864ea2bf07cbcda9472670df6087ea5c5d2
marcosun <marcoinbristol@gmail.com>
2019-03-10 22:23:32 +0800

    [core] Create local, dev and test environment webpack config files.

---
+++ config/webpack.local.js
@@ -0,0 +1,20 @@
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const path = require('./path');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  mode: 'development',
+
+  devtool: 'eval-source-map',
+
+  devServer: {
+    contentBase: path.appDist,
+    historyApiFallback: true,
+    hot: true,
+  },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(), // 热替换插件
+  ],
+});
--- config/webpack.test.js
+++ config/webpack.test.js
@@ -2,7 +2,7 @@ const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
-  mode: 'development',
+  mode: 'production',
 
   devtool: 'source-map',
 });
